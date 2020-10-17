2d7def442bc6f3bff6181ccfde7f9214550a7d9e
Sebastiano Panichella <panc@zhaw.ch>
2020-10-17 23:07:04 +0200

    added code ML components

---
+++ ML_Component/Eclipse-project/ML-pipeline/src/main/java/org/zhaw/ch/ml/MachineLearningClassifier.java
@@ -0,0 +1,32 @@
+package org.zhaw.ch.ml;
+
+
+/**
+ * 
+ * @author panc
+ *
+ */
+public abstract class  MachineLearningClassifier {
+
+	protected static String classifierToolChain;
+	
+	protected static String machineLearningModelName;
+
+	public static String getClassifierToolChain() {
+		return classifierToolChain;
+	}
+
+	public void setClassifierToolChain(String classifierToolChain) {
+		this.classifierToolChain = classifierToolChain;
+	}
+
+	public static String getMachineLearningModelName() {
+		return machineLearningModelName;
+	}
+
+	public void setMachineLearningModelName(String machineLearningModelName) {
+		this.machineLearningModelName = machineLearningModelName;
+	}
+	
+	
+}
