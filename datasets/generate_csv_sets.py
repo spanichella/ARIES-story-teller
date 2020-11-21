4a756162fc6528f1c7dd720d39cfd389997909b5
jluech <janik.luechinger@uzh.ch>
2020-11-21 21:32:07 +0100

    refactor: simplify dataset handling and cleanup script

--- datasets/generate_csv_sets.py
+++ datasets/generate_csv_sets.py
@@ -5,13 +5,18 @@ import random
 REPO_PATH = "/repos/python_playground"
 FILES_MAIN_PATH = "/repos/SWME_G2_HS20/datasets"
 
-STORIES_TRUTH_SET_TXT = FILES_MAIN_PATH + "/stories/truth_set_stories-ReqSpec.txt"
-RECORDING_TRUTH_SET_TXT = FILES_MAIN_PATH + "/recording/truth_set_recording-ReqSpec.txt"
+TDM_TRUTH_SET_PATTERN = "/{dataset}/tdm_truth_set_{dataset}_with_oracle.csv"
+TXT_TRUTH_SET_PATTERN = "/{dataset}/truth_set_{dataset}-ReqSpec.txt"
+RECORDING_TRUTH_SET_TDM = FILES_MAIN_PATH + TDM_TRUTH_SET_PATTERN.format(dataset="recording")
+RECORDING_TRUTH_SET_TXT = FILES_MAIN_PATH + TXT_TRUTH_SET_PATTERN.format(dataset="recording")
+STORIES_TRUTH_SET_TDM = FILES_MAIN_PATH + TDM_TRUTH_SET_PATTERN.format(dataset="stories")
+STORIES_TRUTH_SET_TXT = FILES_MAIN_PATH + TXT_TRUTH_SET_PATTERN.format(dataset="stories")
+DATASETS_DICT = {
+    "recording": {"TDM": RECORDING_TRUTH_SET_TDM, "TXT": RECORDING_TRUTH_SET_TXT},
+    "stories": {"TDM": STORIES_TRUTH_SET_TDM, "TXT": STORIES_TRUTH_SET_TXT}
+}
 VALUE_SEPARATOR_CHAR = "\t"
 SET_CSV_HEADERS = ["id", "req_specification", "class"]
-
-STORIES_TRUTH_SET_TDM = FILES_MAIN_PATH + "/stories/tdm_truth_set_stories_with_oracle.csv"
-RECORDING_TRUTH_SET_TDM = FILES_MAIN_PATH + "/recording/tdm_truth_set_recording_with_oracle.csv"
 
 OUTPUT_PATH = REPO_PATH + "/output"
 OUTPUT_PATH_CSV = OUTPUT_PATH + "/csv_sets"
@@ -31,25 +36,20 @@ def randomly(seq):
     return shuffled
 
 
-def get_truthset_files_dicts():
-    combined_file = open(STORIES_TRUTH_SET_TXT, "r")
-    combined_lines = combined_file.readlines()
-    combined_file.close()
+def get_truth_set_files_dict():
+    truth_set_dict = {}
+    for key in DATASETS_DICT:
+        print(key)
+        file = open(DATASETS_DICT[key]["TXT"], "r")
+        lines = file.readlines()
+        file.close()
+        truth_set_dict[key] = lines
 
-    recording_file = open(RECORDING_TRUTH_SET_TXT, "r")
-    recording_lines = recording_file.readlines()
-    recording_file.close()
-
-    return {"recording": recording_lines, "stories": combined_lines}
+    return truth_set_dict
 
 
-def get_truthset_tdm_path(dataset):
-    if dataset == "recording":
-        return RECORDING_TRUTH_SET_TDM
-    elif dataset == "stories":
-        return STORIES_TRUTH_SET_TDM
-    else:
-        return ""
+def get_truth_set_tdm_path(dataset):
+    return DATASETS_DICT[dataset]["TDM"]
 
 
 def write_specific_csv_file(file_path, headers, content_lines, has_split_values_in_lines=False):
@@ -98,10 +98,7 @@ def write_training_and_test_sets(file_key, separated_content):
 
 
 def write_tdm_file(dataset, file_name, file_path):
-    # print(dataset)
-    tdm_truthset_path = get_truthset_tdm_path(dataset)
-    # print(tdm_truthset_path)
-    # print(file_path)
+    tdm_truthset_path = get_truth_set_tdm_path(dataset)
     tdm_truthset_file = open(tdm_truthset_path, "r")
     tdm_truthset_lines = tdm_truthset_file.readlines()
     tdm_truthset_file.close()
@@ -112,8 +109,6 @@ def write_tdm_file(dataset, file_name, file_path):
         truthset_line_number = tdm_truthset_line.split(",")[0]
         tdm_truthset_content_dict[truthset_line_number] = tdm_truthset_line
 
-    # print(*tdm_truthset_content_dict)
-
     set_csv_file = open(file_path, "r")
     set_csv_lines = set_csv_file.readlines()
     set_csv_file.close()
@@ -121,13 +116,8 @@ def write_tdm_file(dataset, file_name, file_path):
 
     set_tdm_lines = []
     for set_line in set_csv_lines:
-        # print("set line", set_line)
         set_line_number = set_line.split(",")[SET_CSV_HEADERS.index("id")]
-        # print("line number", set_line_number)
         tdm_line = tdm_truthset_content_dict[set_line_number].strip()
-        # print(tdm_line)
-        # if tdm_line[tdm_line.__len__() - 1] == "\n":
-        #     print("has newline at end")
         set_tdm_lines.append(tdm_line)
 
     set_tdm_file_name = TDM_PATTERN.format(file_name=file_name)
@@ -136,7 +126,7 @@ def write_tdm_file(dataset, file_name, file_path):
 
 
 def generate_csv_sets():
-    truthset_files_dicts = get_truthset_files_dicts()
+    truth_set_files_dicts = get_truth_set_files_dict()
 
     if not os.path.exists(OUTPUT_PATH):
         os.mkdir(path=OUTPUT_PATH)
@@ -145,11 +135,11 @@ def generate_csv_sets():
     if not os.path.exists(OUTPUT_PATH_TDM):
         os.mkdir(path=OUTPUT_PATH_TDM)
 
-    for dataset_key in truthset_files_dicts:
-        truthset_content = truthset_files_dicts[dataset_key]
+    for dataset_key in truth_set_files_dicts:
+        truth_set_content = truth_set_files_dicts[dataset_key]
 
         separated_content = []
-        for line in truthset_content:
+        for line in truth_set_content:
             splits = [item.strip().replace('"', "'") for item in line.split(VALUE_SEPARATOR_CHAR)]
             separated_content.append(splits)
 
