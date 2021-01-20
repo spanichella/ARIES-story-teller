f3198b8c9ddaa18763f575a146d0975c91076e29
Nicolas Ganz <gann@zhaw.ch>
2021-01-20 11:56:17 +0100

    ignore empty main method

--- combined-pipelines/src/main/java/UI/SWM_GUI.java
+++ combined-pipelines/src/main/java/UI/SWM_GUI.java
@@ -4,8 +4,8 @@ package UI;
 public class SWM_GUI {
     public static final SWM_Frame frame = new SWM_Frame();
 
+    @SuppressWarnings("EmptyMethod")
     public static void main(String[] args) {
-
     }
 
     public static void killFrames(){
