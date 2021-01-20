9686c05dab2654778ea5d6842ae2d74cb5aa0514
Nicolas Ganz <gann@zhaw.ch>
2021-01-20 10:10:34 +0100

    removed commented out code

--- combined-pipelines/src/main/java/UI/SWM_Frame.java
+++ combined-pipelines/src/main/java/UI/SWM_Frame.java
@@ -508,12 +508,6 @@ public class SWM_Frame extends JFrame implements ActionListener, ItemListener, C
             generateXML();
             Pipeline_Thread mainThread = new Pipeline_Thread(mainPath,args[2],args[1]);
             mainThread.start();
-            /*
-            try {
-                MainPipeline.runPipeline(mainPath, args[2], args[1]);
-            } catch (Exception e1) {
-                System.out.print(e1);
-            }*/
             this.setEnabled(false);
         }
         updateStatus();
