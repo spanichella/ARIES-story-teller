2573d394d9f55247206d3176bf9971baff8201ee
Nicolas Ganz <gann@zhaw.ch>
2021-04-07 15:41:09 +0200

    rename the PipelineThread to match the format

--- combined-pipelines/src/main/java/UI/PipelineThread.java
+++ combined-pipelines/src/main/java/UI/PipelineThread.java
@@ -4,11 +4,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import pipelines.MainPipeline;
 
-public class Pipeline_Thread extends Thread {
-    private final static Logger logger = Logger.getLogger(Pipeline_Thread.class.getName());
+public class PipelineThread extends Thread {
+    private final static Logger logger = Logger.getLogger(PipelineThread.class.getName());
     private final String mainPath, args2, args1;
 
-    Pipeline_Thread(String mainPath, String args2, String args1){
+    PipelineThread(String mainPath, String args2, String args1){
         this.mainPath = mainPath;
         this.args2 = args2;
         this.args1 = args1;
