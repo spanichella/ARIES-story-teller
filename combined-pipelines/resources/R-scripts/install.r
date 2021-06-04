f10bf4b1da1aa7cec898acd1d777ffe4ed2d7a6c
Nicolas Ganz <gann@zhaw.ch>
2021-06-04 11:17:53 +0200

    ensure that the installation errors lead to an abort of the script

--- combined-pipelines/resources/R-scripts/install.r
+++ combined-pipelines/resources/R-scripts/install.r
@@ -1,9 +1,14 @@
+tryInstall = function(pkg) {
+  # stop if the package cant be installed
+  tryCatch(expr={ install.packages(pkg) }, warning=stop)
+}
+
 print("Installing necessary packages...")
-if (!require(tm, quietly=TRUE)){ install.packages("tm") }
-if (!require(stringr, quietly=TRUE)){ install.packages("stringr") }
-if (!require(stopwords, quietly=TRUE, warn.conflicts=FALSE)){ install.packages("stopwords") }
-if (!require(slam, quietly=TRUE)){ install.packages("slam") }
-if (!require(snakecase, quietly=TRUE)){ install.packages("snakecase") }
-if (!require(data.table, quietly=TRUE, warn.conflicts=FALSE)){ install.packages("data.table") }
-if (!require(XML, quietly=TRUE)){ install.packages("XML") }
-if (!require(SnowballC, quietly=TRUE)){ install.packages("SnowballC") }
+if (!require(tm, quietly=TRUE)){ tryInstall("tm") }
+if (!require(stringr, quietly=TRUE)){ tryInstall("stringr") }
+if (!require(stopwords, quietly=TRUE, warn.conflicts=FALSE)){ tryInstall("stopwords") }
+if (!require(slam, quietly=TRUE)){ tryInstall("slam") }
+if (!require(snakecase, quietly=TRUE)){ tryInstall("snakecase") }
+if (!require(data.table, quietly=TRUE, warn.conflicts=FALSE)){ tryInstall("data.table") }
+if (!require(XML, quietly=TRUE)){ tryInstall("XML") }
+if (!require(SnowballC, quietly=TRUE)){ tryInstall("SnowballC") }
