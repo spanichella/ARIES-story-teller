137b774bab19109d8e6fd4d095f4d10a8d6aed28
Nicolas Ganz <gann@zhaw.ch>
2021-06-04 11:15:38 +0200

    fixed the name of the r scripts path

--- combined-pipelines/src/main/java/helpers/CommonPaths.java
+++ combined-pipelines/src/main/java/helpers/CommonPaths.java
@@ -6,6 +6,6 @@ public final class CommonPaths {
     private static final Path MAIN = Path.of(System.getProperty("user.dir"));
     public static final Path PROJECT_ROOT = MAIN.getParent();
     public static final Path RESOURCES = MAIN.resolve("resources");
-    public static final Path R_SCRIPTS = RESOURCES.resolve("R-Scripts");
+    public static final Path R_SCRIPTS = RESOURCES.resolve("R-scripts");
     public static final Path GLOVE_FILE = RESOURCES.resolve("DL").resolve("glove.6B.100d.txt");
 }
